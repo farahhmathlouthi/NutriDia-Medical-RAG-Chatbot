241cae99fc8505e5b5561899193f5b2245be85b2
farahmathlouthi <mathlouthifarah1@gmail.com>
2026-05-15 16:43:12 +0100

    adjusted agents

--- backend/agents/diabetes_agent.py
+++
@@ -1,9 +0,0 @@
-from retrieval.rag_chain import rag_chain
-
-
-class DiabetesAgent:
-    def run(self, query: str):
-        return rag_chain.generate(query)
-
-
-diabetes_agent = DiabetesAgent()
--- backend/agents/nutrition_agent.py
+++
@@ -1,9 +0,0 @@
-from retrieval.rag_chain import rag_chain
-
-
-class NutritionAgent:
-    def run(self, query: str):
-        return rag_chain.generate(query)
-
-
-nutrition_agent = NutritionAgent()
